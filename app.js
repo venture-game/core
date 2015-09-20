ea2241dd5a4b45d80147a5785b88eae4a6d074f1
Max Korinets <mkorinets@gmail.com>
2015-09-20 10:58:17 +0700

    moved `client` up; dropped `public`

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.get('/', function (req, res) {
